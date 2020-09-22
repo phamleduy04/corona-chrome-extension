5108b6ccde0d6f89db7ab8703cc08122e7b04a85
Duy Pham Le <phamleduy04@gmail.com>
2020-09-22 16:17:22 -0500

    upload

--- data.js
+++ data.js
@@ -1,30 +1,31 @@
 $(document).ready(function() {
-    $.get("https://disease.sh/v3/covid-19/all", function(data) {
+    $.get("https://disease.sh/v3/covid-19/all", function(data){
         const { cases, todayCases, deaths, todayDeaths, recovered, todayRecovered, critical } = data;
-        createBodyElement('p', `Cases in the world`)
-        createBodyElement('h1', `Cases: ${soDep(cases)}(+${soDep(todayCases)})`);
-        createBodyElement('h1', `Deaths: ${soDep(deaths)}(+${soDep(todayDeaths)})`);
-        createBodyElement('h1', `Recovered: ${soDep(recovered)}(+${soDep(todayRecovered)})`);
-        createBodyElement('h1', `Critical: ${soDep(critical)}`);
-    });
-    $.get("https://disease.sh/v3/covid-19/countries/vietnam", function(data) {
-        const { cases, todayCases, deaths, todayDeaths, recovered, todayRecovered, critical } = data;
-        createBodyElement('p', "Số ca ở Việt nam");
-        createBodyElement('h1', `Số ca: ${soDep(cases)}(+${soDep(todayCases)})`);
-        createBodyElement('h1', `Tử vong: ${soDep(deaths)}(+${soDep(todayDeaths)})`);
-        createBodyElement('h1', `Hồi phục: ${soDep(recovered)}(+${soDep(todayRecovered)})`);
-        createBodyElement('h1', `Ca nghiêm trọng: ${soDep(critical)}`);
+        bodyEl('p', "Số ca corona ở Thế Giới");
+        bodyEl('h1', `Số ca nhiễm: ${dep(cases)}(+${dep(todayCases)})`);
+        bodyEl('h1', `Tử vong: ${dep(deaths)}(+${dep(todayDeaths)})`);
+        bodyEl('h1', `Hồi phục: ${dep(recovered)}(+${dep(todayRecovered)})`);
+        bodyEl('h1', `Nghiêm trọng: ${dep(critical)}`);
+
+        $.get("https://disease.sh/v3/covid-19/countries/VN?strict=true", function(data){
+            const { cases, todayCases, deaths, todayDeaths, recovered, todayRecovered, critical } = data;
+            bodyEl('p', "Số ca corona ở Việt Nam");
+            bodyEl('h1', `Số ca nhiễm: ${dep(cases)}(+${dep(todayCases)})`);
+            bodyEl('h1', `Tử vong: ${dep(deaths)}(+${dep(todayDeaths)})`);
+            bodyEl('h1', `Hồi phục: ${dep(recovered)}(+${dep(todayRecovered)})`);
+            bodyEl('h1', `Nghiêm trọng: ${dep(critical)}`);
+        })
     })
 })
 
-function createBodyElement(type, text){
+function bodyEl(type, text) {
     const h = document.createElement(type);
-    const t = document.createTextNode(text.replace(' ', "\u00a0"));
+    const t = document.createTextNode(text);
     h.appendChild(t);
     document.body.appendChild(h);
 }
 
-function soDep(num) {
-    if (!num || num == null) return 0;
-    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+function dep(num) {
+    if (!num || num === null) return 0;
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
 }
