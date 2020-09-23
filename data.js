3c9893cbf1f0c4a86ceabce4e6e5f735daec6904
Duy Pham Le <phamleduy04@gmail.com>
2020-09-22 21:37:47 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,21 +1,16 @@
-$(document).ready(function() {
-    $.get("https://disease.sh/v3/covid-19/all", function(data){
+$(document).ready(async function() {
+    async function setBody(url, title) {
+        const data = await $.get(url);
         const { cases, todayCases, deaths, todayDeaths, recovered, todayRecovered, critical } = data;
-        bodyEl('p', "Số ca corona ở Thế Giới");
+        bodyEl('p', title);
         bodyEl('h1', `Số ca nhiễm: ${dep(cases)}(+${dep(todayCases)})`);
         bodyEl('h1', `Tử vong: ${dep(deaths)}(+${dep(todayDeaths)})`);
         bodyEl('h1', `Hồi phục: ${dep(recovered)}(+${dep(todayRecovered)})`);
         bodyEl('h1', `Nghiêm trọng: ${dep(critical)}`);
+    }
 
-        $.get("https://disease.sh/v3/covid-19/countries/VN?strict=true", function(data){
-            const { cases, todayCases, deaths, todayDeaths, recovered, todayRecovered, critical } = data;
-            bodyEl('p', "Số ca corona ở Việt Nam");
-            bodyEl('h1', `Số ca nhiễm: ${dep(cases)}(+${dep(todayCases)})`);
-            bodyEl('h1', `Tử vong: ${dep(deaths)}(+${dep(todayDeaths)})`);
-            bodyEl('h1', `Hồi phục: ${dep(recovered)}(+${dep(todayRecovered)})`);
-            bodyEl('h1', `Nghiêm trọng: ${dep(critical)}`);
-        })
-    })
+    await setBody('https://disease.sh/v3/covid-19/all', "Số ca corona ở Thế Giới");
+    await setBody('https://disease.sh/v3/covid-19/countries/VN?strict=true', "Số ca corona ở Việt Nam");
 })
 
 function bodyEl(type, text) {
@@ -29,3 +24,4 @@ function dep(num) {
     if (!num || num === null) return 0;
     return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
 }
+
