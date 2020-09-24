51bdb94e3c307ae145bece2d1a2b58e1ffc03174
Duy Pham Le <phamleduy04@gmail.com>
2020-09-23 21:54:22 -0700

    upload

---
+++ settings.js
@@ -0,0 +1,23 @@
+$(document).ready(async function() {
+    const countriesArray = await $.get('https://disease.sh/v3/covid-19/countries');
+    selectBody(countriesArray.map(el => el.country));
+
+    document.getElementById('save').addEventListener("click", () => {
+        const userChoose = $('#select').val();
+        alert(`You choose: ${userChoose}`);
+    })
+})
+
+function selectBody(array) {
+    const myP = document.getElementById('selectList');
+    const selectList = document.createElement('select');
+    selectList.setAttribute("id", "select");
+    myP.appendChild(selectList);
+
+    array.forEach(el => {
+        const option = document.createElement('option');
+        option.setAttribute("value", el);
+        option.text = el;
+        selectList.appendChild(option);
+    })
+}
